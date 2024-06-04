e586bb06211e4e26b2490dd6ea612a01a968fe2f
Sage9705 <edem22kumahor@gmail.com>
2024-06-04 14:56:58 +0100

    use task routes

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/tasks', require('./routes/tasks'));
 
 // Basic route
 app.get('/', (req, res) => {
