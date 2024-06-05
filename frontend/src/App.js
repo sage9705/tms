eebb17338fa7ba243bea28f547e52a5bc382f087
Sage9705 <edem22kumahor@gmail.com>
2024-06-06 00:09:58 +0100

    add styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AuthProvider } from './contexts/authContext';
+import { TaskProvider } from './contexts/TaskContext';
+import PrivateRoute from './components/privateRoute';
 import Login from './components/auth/login';
 import Register from './components/auth/register';
 import TaskList from './components/tasks/tasklist';
-import PrivateRoute from './components/privateRoute';
 
-const App = () => {
+function App() {
     return (
-        <Router>
-            <div className="App">
-                <Switch>
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/register" component={Register} />
-                    <PrivateRoute exact path="/" component={TaskList} />
-                </Switch>
-            </div>
-        </Router>
+        <AuthProvider>
+            <TaskProvider>
+                <Router>
+                    <div className="App">
+                        <Switch>
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/register" component={Register} />
+                            <PrivateRoute exact path="/tasks" component={TaskList} />
+                        </Switch>
+                    </div>
+                </Router>
+            </TaskProvider>
+        </AuthProvider>
     );
-};
+}
 
 export default App;
